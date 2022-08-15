253cd8ab2f5111bb4ab286d13e3d1b31005a1e44
Amtresu <conor.follette@gmail.com>
2022-08-15 16:43:31 +0900

    added my code for peer review

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// eslint-disable-next-line no-unused-vars
+import _, { update } from 'lodash'
+import './style.css'
+import arrow from '../src/assets/images/add-item-img.png'
+import refresh from '../src/assets/images/refresh.png'
+import { display, addToDo, clear, clearAll } from './CRUD.js'
+
+document.querySelector('.refresh').src = `${refresh}`
+document.querySelector('.arrow').src = `${arrow}`
+
+const refreshImg = document.querySelector('.refresh')
+
+const form = document.querySelector('.form')
+const input = document.querySelector('.add-item')
+const arrowImg = document.querySelector('.arrow')
+const clearCompleted = document.querySelector('.remove-button')
+const toDoList = document.querySelector('.to-do-list-ul')
+
+window.addEventListener('load', () => {
+  const toDoList = document.querySelector('.to-do-list-ul')
+  display(toDoList)
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const toDoList = document.querySelector('.to-do-list-ul')
+  const inputVal = input.value
+  addToDo(inputVal)
+  input.value = ''
+  display(toDoList)
+})
+
+arrowImg.addEventListener('click', (e) => {
+  e.preventDefault()
+  const toDoList = document.querySelector('.to-do-list-ul')
+  const inputVal = input.value
+  addToDo(inputVal)
+  input.value = ''
+  display(toDoList)
+})
+
+clearCompleted.addEventListener('click', () => {
+  clear(toDoList)
+})
+
+refreshImg.addEventListener('click', () => {
+  clearAll(toDoList)
+})
